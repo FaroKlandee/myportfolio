245a28c652560998e63be588988f3021fc1d61c9
Patiphak Klandee <f.klandee@gmail.com>
2025-04-09 21:49:39 +1000

    Updated browser tab icon, added new link for CV

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,17 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Faro Klandee | Developer for Freedom",
   description: "Portfolio of Faro Klandee, a developer specializing in creating profitable software",
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: 'favicon-16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: 'favicon-32x32', type: 'image/png' },
+      { url: '/favicon-192x192.png', sizes: 'favicon-192x192', type: 'image/png' },
+      { url: '/favicon-512x512.png', sizes: 'favicon-512x512', type: 'image/png' },
+    ],
+    // For Apple devices
+    apple: '/favicon-apple.png',
+  },
 };
 
 export default function RootLayout({
