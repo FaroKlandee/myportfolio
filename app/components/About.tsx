4fe3ae1d28fad519cbaed11fdf8c969d5c4e5d42
Patiphak Klandee <f.klandee@gmail.com>
2025-06-03 22:46:03 +1000

    added more tech

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import { FiFigma } from "react-icons/fi";
+import { FaJava, FaPython } from "react-icons/fa";
 
 const About = () => {
     const fadeInUp = {
@@ -87,27 +87,65 @@ const About = () => {
                     </p>
 
                     <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-                        <a href="https://www.figma.com/" target="_blank" rel="noopener noreferrer nofollow" className="card flex items-center p-4 hover:border-white/30 transition-colors">
-                            <FiFigma className="text-4xl mr-4" />
+                        <a href="https://figma.com/" target="_blank" rel="noopener noreferrer nofollow"
+                            className="card flex items-center p-4 hover:border-white/30 transition-colors">
+                            <svg className="w-8 h-10 mr-4" viewBox="0 0 1024 1280" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M312 840C312 784.772 356.772 740 412 740H512V840C512 895.228 467.228 940 412 940C356.772 940 312 895.228 312 840Z"
+                                    fill="#24CB71" />
+                                <path d="M512 340V540H612C667.228 540 712 495.228 712 440C712 384.772 667.228 340 612 340H512Z" fill="#FF7237" />
+                                <circle cx="611.167" cy="640" r="100" fill="#00B6FF" />
+                                <path d="M312 440C312 495.228 356.772 540 412 540H512V340H412C356.772 340 312 384.772 312 440Z" fill="#FF3737" />
+                                <path d="M312 640C312 695.228 356.772 740 412 740H512V540H412C356.772 540 312 584.772 312 640Z" fill="#874FFF" />
+                            </svg>
                             <div>
                                 <h4 className="font-medium">Figma</h4>
                                 <p className="text-sm text-white/50">UI/UX Design & Prototyping</p>
                             </div>
                         </a>
 
-                        <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer nofollow" className="card flex items-center p-4 hover:border-white/30 transition-colors">
-                            <svg className="text-4xl mr-4 w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M11.572 0c-.176 0-.31.001-.358.007a19.76 19.76 0 0 1-.364.033C7.443.346 4.25 2.185 2.228 5.012a11.875 11.875 0 0 0-2.119 5.243c-.096.659-.108.854-.108 1.747s.012 1.089.108 1.748c.652 4.506 3.86 8.292 8.209 9.695.779.25 1.6.422 2.534.525.363.04 1.935.04 2.299 0 1.611-.178 2.977-.577 4.323-1.264.207-.106.247-.134.219-.158-.02-.013-.9-1.193-1.955-2.62l-1.919-2.592-2.404-3.558a338.739 338.739 0 0 0-2.422-3.556c-.009-.002-.018 1.579-.023 3.51-.007 3.38-.01 3.515-.052 3.595a.426.426 0 0 1-.206.214c-.075.037-.14.044-.495.044H7.81l-.108-.068a.438.438 0 0 1-.157-.171l-.05-.106.006-4.703.007-4.705.072-.092a.645.645 0 0 1 .174-.143c.096-.047.134-.051.54-.051.478 0 .558.018.682.154.035.038 1.337 1.999 2.895 4.361a10760.433 10760.433 0 0 0 4.735 7.17l1.9 2.879.096-.063a12.317 12.317 0 0 0 2.466-2.163 11.944 11.944 0 0 0 2.824-6.134c.096-.66.108-.854.108-1.748 0-.893-.012-1.088-.108-1.747-.652-4.506-3.859-8.292-8.208-9.695a12.597 12.597 0 0 0-2.499-.523A33.119 33.119 0 0 0 11.573 0zm4.069 7.217c.347 0 .408.005.486.047a.473.473 0 0 1 .237.277c.018.06.023 1.365.018 4.304l-.006 4.218-.744-1.14-.746-1.14v-3.066c0-1.982.01-3.097.023-3.15a.478.478 0 0 1 .233-.296c.096-.05.13-.054.5-.054z" />
+                        <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer nofollow"
+                            className="card flex items-center p-4 hover:border-white/30 transition-colors">
+                            <svg className="mr-4" width="32" height="32" viewBox="0 0 180 180" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <mask id="mask0_nextjs" style={{ maskType: 'alpha' }} maskUnits="userSpaceOnUse" x="0" y="0" width="180" height="180">
+                                    <circle cx="90" cy="90" r="90" fill="black" />
+                                </mask>
+                                <g mask="url(#mask0_nextjs)">
+                                    <circle cx="90" cy="90" r="87" fill="black" stroke="white" strokeWidth="6" />
+                                    <path d="M149.508 157.52L69.142 54H54V125.97H66.1136V69.3836L139.999 164.845C143.333 162.614 146.509 160.165 149.508 157.52Z"
+                                        fill="url(#paint0_linear_nextjs)" />
+                                    <rect x="115" y="54" width="12" height="72" fill="url(#paint1_linear_nextjs)" />
+                                </g>
+                                <defs>
+                                    <linearGradient id="paint0_linear_nextjs" x1="109" y1="116.5" x2="144.5" y2="160.5" gradientUnits="userSpaceOnUse">
+                                        <stop stopColor="white" />
+                                        <stop offset="1" stopColor="white" stopOpacity="0" />
+                                    </linearGradient>
+                                    <linearGradient id="paint1_linear_nextjs" x1="121" y1="54" x2="120.799" y2="106.875" gradientUnits="userSpaceOnUse">
+                                        <stop stopColor="white" />
+                                        <stop offset="1" stopColor="white" stopOpacity="0" />
+                                    </linearGradient>
+                                </defs>
                             </svg>
                             <div>
                                 <h4 className="font-medium">Next.js</h4>
-                                <p className="text-sm text-white/50">web development framework</p>
+                                <p className="text-sm text-white/50">Web development framework</p>
                             </div>
                         </a>
 
-                        <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer nofollow" className="card flex items-center p-4 hover:border-white/30 transition-colors">
-                            <svg className="text-4xl mr-4 w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z" />
+
+                        <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer nofollow"
+                            className="card flex items-center p-4 hover:border-white/30 transition-colors">
+                            <svg className="mr-4" width="32" height="20" viewBox="0 0 54 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clipPath="url(#prefix__clip0)">
+                                    <path fill="#14B4C6" fillRule="evenodd"
+                                        d="M27 0c-7.2 0-11.7 3.6-13.5 10.8 2.7-3.6 5.85-4.95 9.45-4.05 2.054.513 3.522 2.004 5.147 3.653C30.744 13.09 33.808 16.2 40.5 16.2c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C36.756 3.11 33.692 0 27 0zM13.5 16.2C6.3 16.2 1.8 19.8 0 27c2.7-3.6 5.85-4.95 9.45-4.05 2.054.514 3.522 2.004 5.147 3.653C17.244 29.29 20.308 32.4 27 32.4c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C23.256 19.31 20.192 16.2 13.5 16.2z"
+                                        clipRule="evenodd" />
+                                </g>
+                                <defs>
+                                    <clipPath id="prefix__clip0">
+                                        <path fill="#fff" d="M0 0h54v32.4H0z" />
+                                    </clipPath>
+                                </defs>
                             </svg>
                             <div>
                                 <h4 className="font-medium">Tailwind CSS</h4>
@@ -116,8 +154,21 @@ const About = () => {
                         </a>
 
                         <a href="https://neon.tech/" target="_blank" rel="noopener noreferrer nofollow" className="card flex items-center p-4 hover:border-white/30 transition-colors">
-                            <svg className="text-4xl mr-4 w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M0 12C0 5.373 5.373 0 12 0c4.873 0 9.067 2.904 10.947 7.077l-15.87 15.87a11.981 11.981 0 0 1-1.935-1.099L14.99 12H12l-8.485 8.485A11.962 11.962 0 0 1 0 12Zm12.004 12L24 12.004C23.998 18.628 18.628 23.998 12.004 24Z" />
+                            <svg className="mr-4" width="32" height="20" viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M0 10C0 4.47715 4.47705 0 9.99976 0H47.9989C53.5216 0 57.9986 4.47715 57.9986 10V42.3189C57.9986 48.0326 50.7684 50.5124 47.2618 46.0014L36.2991 31.8988V49C36.2991 53.9706 32.2698 58 27.2993 58H9.99976C4.47705 58 0 53.5228 0 48V10ZM9.99976 8C8.89522 8 7.99981 8.89543 7.99981 10V48C7.99981 49.1046 8.89522 50 9.99976 50H27.5993C28.1516 50 28.2993 49.5523 28.2993 49V26.0673C28.2993 20.3536 35.5295 17.8738 39.0361 22.3848L49.9988 36.4874V10C49.9988 8.89543 50.1034 8 48.9988 8H9.99976Z" fill="#12FFF7" />
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M0 10C0 4.47715 4.47705 0 9.99976 0H47.9989C53.5216 0 57.9986 4.47715 57.9986 10V42.3189C57.9986 48.0326 50.7684 50.5124 47.2618 46.0014L36.2991 31.8988V49C36.2991 53.9706 32.2698 58 27.2993 58H9.99976C4.47705 58 0 53.5228 0 48V10ZM9.99976 8C8.89522 8 7.99981 8.89543 7.99981 10V48C7.99981 49.1046 8.89522 50 9.99976 50H27.5993C28.1516 50 28.2993 49.5523 28.2993 49V26.0673C28.2993 20.3536 35.5295 17.8738 39.0361 22.3848L49.9988 36.4874V10C49.9988 8.89543 50.1034 8 48.9988 8H9.99976Z" fill="url(#paint0_linear_10558_11777)" />
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M0 10C0 4.47715 4.47705 0 9.99976 0H47.9989C53.5216 0 57.9986 4.47715 57.9986 10V42.3189C57.9986 48.0326 50.7684 50.5124 47.2618 46.0014L36.2991 31.8988V49C36.2991 53.9706 32.2698 58 27.2993 58H9.99976C4.47705 58 0 53.5228 0 48V10ZM9.99976 8C8.89522 8 7.99981 8.89543 7.99981 10V48C7.99981 49.1046 8.89522 50 9.99976 50H27.5993C28.1516 50 28.2993 49.5523 28.2993 49V26.0673C28.2993 20.3536 35.5295 17.8738 39.0361 22.3848L49.9988 36.4874V10C49.9988 8.89543 50.1034 8 48.9988 8H9.99976Z" fill="url(#paint1_linear_10558_11777)" />
+                                <path d="M48.0003 0C53.523 0 58 4.47715 58 10V42.3189C58 48.0326 50.7699 50.5124 47.2633 46.0014L36.3006 31.8988V49C36.3006 53.9706 32.2712 58 27.3008 58C27.8531 58 28.3008 57.5523 28.3008 57V26.0673C28.3008 20.3536 35.5309 17.8738 39.0375 22.3848L50.0002 36.4874V2C50.0002 0.89543 49.1048 0 48.0003 0Z" fill="#B9FFB3" />
+                                <defs>
+                                    <linearGradient id="paint0_linear_10558_11777" x1="57.9986" y1="58" x2="6.99848" y2="0.00123034" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#B9FFB3" />
+                                        <stop offset="1" stop-color="#B9FFB3" stop-opacity="0" />
+                                    </linearGradient>
+                                    <linearGradient id="paint1_linear_10558_11777" x1="57.9986" y1="58" x2="23.5492" y2="44.6006" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#1A1A1A" stop-opacity="0.9" />
+                                        <stop offset="1" stop-color="#1A1A1A" stop-opacity="0" />
+                                    </linearGradient>
+                                </defs>
                             </svg>
                             <div>
                                 <h4 className="font-medium">Neon Postgres</h4>
@@ -125,9 +176,21 @@ const About = () => {
                             </div>
                         </a>
 
+
                         <a href="https://next-auth.js.org/" target="_blank" rel="noopener noreferrer nofollow" className="card flex items-center p-4 hover:border-white/30 transition-colors">
-                            <svg className="text-4xl mr-4 w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm-.041 4.2c.85.002 1.653.374 2.2 1.009.533.628.759 1.45.624 2.264a2.86 2.86 0 0 1-1.228 1.98 2.836 2.836 0 0 1-2.273.42 2.86 2.86 0 0 1-1.98-1.229 2.835 2.835 0 0 1-.42-2.273 2.86 2.86 0 0 1 1.228-1.98 2.835 2.835 0 0 1 1.849-.19zm6.772 3.448a.868.868 0 0 1 .913.817c.01.08.011.16.003.24-.046.46-.236.885-.54 1.198a14.85 14.85 0 0 1-2.812 2.107c-.116.063-.232.125-.348.185a.872.872 0 0 1-.83.045.866.866 0 0 1-.542-.649.873.873 0 0 1 .32-.975c.132-.092.268-.176.405-.265.91-.592 1.746-1.305 2.467-2.122.195-.22.402-.43.626-.622.112-.096.235-.179.37-.237a.87.87 0 0 1 .968.278zm-8.306 3.97c2.08 0 3.768 1.687 3.768 3.766 0 2.08-1.688 3.766-3.768 3.766-2.08 0-3.767-1.687-3.767-3.766 0-2.08 1.688-3.767 3.767-3.767zm8.309 1.603c.85.002 1.653.374 2.2 1.009.533.628.759 1.45.624 2.264a2.86 2.86 0 0 1-1.228 1.98 2.836 2.836 0 0 1-2.273.42 2.86 2.86 0 0 1-1.98-1.229 2.835 2.835 0 0 1-.42-2.273 2.86 2.86 0 0 1 1.228-1.98 2.835 2.835 0 0 1 1.849-.19z" />
+                            <svg className="mr-4" width="32" height="28" viewBox="0 0 210 232" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M208.687 31.859L30.0947 167.73C8.16388 127.966 0.567559 79.9975 0 51.2275V33.6181C0 31.0567 2.7788 29.5628 4.16851 29.1356C33.5617 20.2778 93.2465 2.30524 96.8378 1.28069C100.429 0.256138 103.464 0 104.534 0H104.631C105.7 0 108.735 0.256138 112.327 1.28069C115.918 2.30524 175.603 20.2778 204.996 29.1356C206.03 29.4533 207.832 30.3613 208.687 31.859Z" fill="url(#paint0_linear_128_61)" />
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M30.0938 167.952L208.686 32.0815C208.981 32.5983 209.164 33.1842 209.164 33.8406V51.45C208.202 100.223 187.038 204.172 110.081 229.786C109.013 230.213 106.49 231.067 104.951 231.067H104.212C102.673 231.067 100.150 230.213 99.0814 229.786C67.5193 219.281 45.3415 195.6 30.0938 167.952Z" fill="url(#paint1_linear_128_61)" />
+                                <defs>
+                                    <linearGradient id="paint0_linear_128_61" x1="104.582" y1="0" x2="104.582" y2="231.305" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#7C3AED" />
+                                        <stop offset="1" stop-color="#3B82F6" />
+                                    </linearGradient>
+                                    <linearGradient id="paint1_linear_128_61" x1="104.582" y1="0" x2="104.582" y2="231.305" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#7C3AED" />
+                                        <stop offset="1" stop-color="#3B82F6" />
+                                    </linearGradient>
+                                </defs>
                             </svg>
                             <div>
                                 <h4 className="font-medium">NextAuth</h4>
@@ -135,9 +198,11 @@ const About = () => {
                             </div>
                         </a>
 
-                        <a href="https://stripe.com/" target="_blank" rel="noopener noreferrer nofollow" className="card flex items-center p-4 hover:border-white/30 transition-colors">
-                            <svg className="text-4xl mr-4 w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M13.976 9.15c-2.172-.806-3.356-1.426-3.356-2.409 0-.831.683-1.305 1.901-1.305 2.227 0 4.515.858 6.09 1.631l.89-5.494C18.252.975 15.697 0 12.165 0 9.667 0 7.589.654 6.104 1.872 4.56 3.147 3.757 4.992 3.757 7.218c0 4.039 2.467 5.76 6.476 7.219 2.585.92 3.445 1.574 3.445 2.583 0 .98-.84 1.545-2.354 1.545-1.875 0-4.965-.921-6.99-2.109l-.9 5.555C5.175 22.99 8.385 24 11.714 24c2.641 0 4.843-.624 6.328-1.813 1.664-1.305 2.525-3.236 2.525-5.732 0-4.128-2.524-5.851-6.594-7.305h.003z" />
+                        <a href="https://stripe.com/" target="_blank" rel="noopener noreferrer nofollow"
+                            className="card flex items-center p-4 hover:border-white/30 transition-colors">
+                            <svg className="mr-4" width="32" height="32" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" clipRule="evenodd" fill="#635bff" d="M0 0h400v400H0z" />
+                                <path d="M184.4 155.5c0-9.4 7.7-13.1 20.5-13.1 18.4 0 41.6 5.6 60 15.5v-56.8C244.8 93.1 225 90 205 90c-49.1 0-81.7 25.6-81.7 68.4 0 66.7 91.9 56.1 91.9 84.9 0 11.1-9.7 14.7-23.2 14.7-20.1 0-45.7-8.2-66-19.3v57.5c22.5 9.7 45.2 13.8 66 13.8 50.3 0 84.9-24.9 84.9-68.2-.4-72-92.5-59.2-92.5-86.3z" fillRule="evenodd" clipRule="evenodd" fill="#fff" />
                             </svg>
                             <div>
                                 <h4 className="font-medium">Stripe</h4>
@@ -151,6 +216,22 @@ const About = () => {
                             <div>
                                 <h4 className="font-medium">MongoDB</h4>
                                 <p className="text-sm text-white/50">Versatile Database</p>
+                            </div>
+                        </a>
+                        <a href="https://www.oracle.com/java/" target="_blank" rel="noopener noreferrer nofollow"
+                            className="card flex items-center p-4 hover:border-white/30 transition-colors">
+                            <FaJava className="mr-4 w-8 h-8" style={{ color: '#f89820' }} />
+                            <div>
+                                <h4 className="font-medium">Java</h4>
+                                <p className="text-sm text-white/50">OOP language</p>
+                            </div>
+                        </a>
+                        <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer nofollow"
+                            className="card flex items-center p-4 hover:border-white/30 transition-colors">
+                            <FaPython className="mr-4 w-8 h-8" style={{ color: '#3776ab' }} />
+                            <div>
+                                <h4 className="font-medium">Python</h4>
+                                <p className="text-sm text-white/50">Programming Language</p>
                             </div>
                         </a>
                     </div>
